6a1af666ece83b04045d9e297645e6e84c9e4aae
Myles Voigt <myles.voigt@gmail.com>
2023-04-05 18:02:34 -0500

    made it so that you can create and admin account for us the developers

--- database.sql
+++ database.sql
@@ -30,18 +30,18 @@ CREATE TABLE "job" (
     "claimed" BOOLEAN,
     "property_id" INT REFERENCES "property",
     "owner_id" INT REFERENCES "user",
-    "keeper_id" INT REFERENCES "user",
+    "keeper_id" INT REFERENCES "user"
 );
 
 CREATE TABLE "checklist_item" (
     "id" SERIAL PRIMARY KEY,
     "completed" BOOLEAN,
-    "user_id" INT REFERENCES "user",
+    "user_id" INT REFERENCES "user"
 );
 
 CREATE TABLE "job_checklist" (
     "id" SERIAL PRIMARY KEY,
     "job_id" INT REFERENCES "job",
-    "checklist_item_id" INT REFERENCES "checklist_item",
+    "checklist_item_id" INT REFERENCES "checklist_item"
 );
 
