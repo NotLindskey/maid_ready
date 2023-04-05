bd851e0b8f475d9d60adf0b857bec3532326cdba
Peter Lee <PeterLee2094@gmail.com>
2023-04-05 18:43:25 -0500

    added useHistory push to navigate to correct pages

--- src/components/LoginSelectionPage/LoginSelectionPage.jsx
+++ src/components/LoginSelectionPage/LoginSelectionPage.jsx
@@ -6,11 +6,11 @@ function LoginSelection(){
     return(
         <div className="login-register-selection-body">
             <div className='login-owner'>
-                <button>Sign in/up as an Owner</button>
+                <button onClick={()=>{history.push('/login/owner')}}>Sign in/up as an Owner</button>
             </div>
 
             <div className='login-keeper'>
-                <button>Sign in/up as a Keeper</button>
+                <button  onClick={()=>{history.push('/login/keeper')}}>Sign in/up as a Keeper</button>
             </div>
         </div>
     )
