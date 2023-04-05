ab875142fdc9c144d1dea12f00289a8fb796f288
Linssi Colin Ruiz <linssi101@gmail.com>
2023-04-05 00:12:46 -0500

    coped registration page for specifically owners

---
+++ src/components/RegisterPage/RegisterOwnerPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { useHistory } from 'react-router-dom';
+import RegisterForm from '../RegisterForm/RegisterForm';
+
+function RegisterOwnerPage() {
+  const history = useHistory();
+
+  return (
+    <div>
+      <h2>Welcome Owner</h2>
+      <p>Register here:</p>
+      <RegisterForm />
+
+      <center>
+        <button
+          type="button"
+          className="btn btn_asLink"
+          onClick={() => {
+            history.push('/login');
+          }}
+        >
+          Login
+        </button>
+      </center>
+    </div>
+  );
+}
+
+export default RegisterOwnerPage;
