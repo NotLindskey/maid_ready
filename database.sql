b565376281992a7ec1f1284e7aecd6ae5d0da532
Peter Lee <PeterLee2094@gmail.com>
2023-04-04 19:26:33 -0500

    add comment to database.sql

--- database.sql
+++ database.sql
@@ -3,6 +3,8 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
+-- testing out git branching
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
