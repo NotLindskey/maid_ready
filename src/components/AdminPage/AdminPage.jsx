affd8bf0b0b66938f5ea9a259443ba3a4cdd6cde
Myles Voigt <myles.voigt@gmail.com>
2023-04-04 20:41:57 -0500

    basic setup of AdminPage

---
+++ src/components/AdminPage/AdminPage.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+ 
+ function AdminPage() {
+    return(
+        <>
+            Admin Page
+        </>
+    )
+ }
+
+ export default AdminPage;
