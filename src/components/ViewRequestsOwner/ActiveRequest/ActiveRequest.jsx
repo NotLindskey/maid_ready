af74f30c1e00f59f261760891abe70bc67f1bb08
Linssi Colin Ruiz <linssi101@gmail.com>
2023-04-08 18:24:27 -0500

    created active request layout

---
+++ src/components/ViewRequestsOwner/ActiveRequest/ActiveRequest.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function ActiveRequest() {
+  console.log('in active request component');
+
+  return (
+    <div>
+      <div className="active-request-header">
+        Property Name Here
+        <div className="active-request-content">
+          <div className="active-request-image">Insert Image Property Here</div>
+          <div className="active-request-details">
+            <ul>
+              <li>Name</li>
+              <li>address</li>
+              <li>miles</li>
+              <li>dates</li>
+              <li>price</li>
+            </ul>
+            <button>View</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ActiveRequest;
