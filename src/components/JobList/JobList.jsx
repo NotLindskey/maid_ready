7d90b5a7a12707848ec5d632bf338140c842921c
Peter Lee <PeterLee2094@gmail.com>
2023-04-09 15:27:51 -0500

    added grid to page, with some alignment and box shadows

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -3,7 +3,18 @@ import JobItem from '../JobItem/JobItem';
 function JobList(){
     return(
         <div className='job-list-body'>
+            <div className='job-list-container'>
             <JobItem/>
+            <JobItem/>
+            <JobItem/>
+            <JobItem/>
+            <JobItem/>
+            <JobItem/>
+            <JobItem/>
+            <JobItem/>
+            <JobItem/>
+            </div>
+
         </div>
     )
 }
