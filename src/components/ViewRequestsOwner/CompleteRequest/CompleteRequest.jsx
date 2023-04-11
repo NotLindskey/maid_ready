02601b7b40f7cf186368cae01c7fef58709f9967
Linssi Colin Ruiz <linssi101@gmail.com>
2023-04-10 21:19:37 -0500

    created CompletedRequest html layout

---
+++ src/components/ViewRequestsOwner/CompleteRequest/CompleteRequest.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function CompleteRequest() {
+  console.log('in active request component');
+
+  return (
+    <div>
+      <div className="complete-request-header">
+        Property Name Here
+        <div className="complete-request-content">
+          <div className="complete-request-image">
+            Insert Image Property Here
+          </div>
+          <div className="complete-request-details">
+            <ul>
+              <li>Name</li>
+              <li>address</li>
+              <li>miles</li>
+              <li>dates</li>
+              <li>price</li>
+            </ul>
+            <button>View</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CompleteRequest;
