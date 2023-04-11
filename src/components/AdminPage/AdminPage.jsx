37b8bd8ea5e2bf02eca8d99078c493409a02fce9
Myles Voigt <myles.voigt@gmail.com>
2023-04-11 17:47:41 -0500

    added a modal for the AdminPage

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,25 +1,40 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import './AdminPage.css';
+import Modal from './AdminPageModal';
  
  function AdminPage() {
     const dispatch = useDispatch();
     const [userName, setUserName] = useState('');
     const [accountType, setAccountType] = useState('');
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [openModal, setOpenModal] = useState(false);
 
     //handles when the user clicks the add-btn
     const handleAdd = () => {
-        //pop up modal to get password and email for the admin
-        // dispatch({type: 'REGISTER', payload: });
+        if(accountType == 'admin'){
+            // pop up modal to get password and email for the admin to be added
+            setOpenModal(true);
+
+            // dispatch({type: 'REGISTER', payload: {
+            //     username: userName,
+            //     account_type: accountType,
+            //     email: email,
+            //     password: password
+            //     }
+            // });
+        }else if (accountType == ''){
+            alert('Please select a user type.');
+        }
     }
 
     //handles when the user clicks the remove-btn
     const handleRemove = () => {
         //check if required fields are filled in
-        if (manageUser == ''){
+        if (userName == ''){
             alert('Please enter a user name.');
-        }else if (userType == ''){
+        }else if (accountType == ''){
             alert('Please select a user type.');
         }
         //remove the user
@@ -31,32 +46,41 @@ import './AdminPage.css';
         }
     }
 
-    const toggleAddBtn = (type, value) => {
-        setManageUser(type);
+    //function to handle when a radio button is clicked
+    const handleRadioBtnClick = (type, value) => {
+        //set account type
+        setAccountType(type);
+        //toggle the add-btn
         document.getElementById("add-btn").disabled = value;
+
     }
 
     return(
         <div>
-            <h3>Manage Users</h3>
+            {openModal && <Modal closeModal={setOpenModal} />}
+            <h3 id="header">Manage Users</h3>
             <br></br>
 
-            <div id="manage-user-inputs">
-                <label> User Name:
-                    <input id="user-name-input" onChange={(event) => setUserName(event.target.value)}></input>
-                </label>
+            <div className="admin-controls">
+                <div id="manage-user-inputs">
+                    <label> User Name:
+                        <input id="user-name-input" placeholder="Enter Username" onChange={(event) => setUserName(event.target.value)}></input>
+                    </label>
+                </div>
+            
+                <div id="radio-btn-div" onChange={(event) => setAccountType(event.target.value)}>
+                    <input type="radio" value="admin" name="user" onChange={() => handleRadioBtnClick('admin', false)}/> Admin <br></br>
+                    <input type="radio" value="owner" name="user" onChange={() => handleRadioBtnClick('owner', true)}/> Owner <br></br>
+                    <input type="radio" value="keeper" name="user" onChange={() => handleRadioBtnClick('keeper', true)}/> Keeper
+                </div>
+
+                <div id="add-remove-btn-div">
+                    <button id="add-btn" className="btn" onClick={() => handleAdd()}>Add</button>
+                    &nbsp;
+                    <button id="remove-btn" className="btn" onClick={() => handleRemove()}>Remove</button>
+                </div>
             </div>
             
-            <div id="radio-btn-div" onChange={(event) => setUserType(event.target.value)}>
-                <input type="radio" value="admin" name="user" onChange={() => toggleAddBtn('admin', false)}/> Admin <br></br>
-                <input type="radio" value="owner" name="user" onChange={() => toggleAddBtn('owner', true)}/> Owner <br></br>
-                <input type="radio" value="keeper" name="user" onChange={() => toggleAddBtn('keeper', true)}/> Keeper
-            </div>
-
-            <div id="add-remove-btn-div">
-                <button id="add-btn">Add</button>
-                <button id="remove-btn" onClick={() => handleRemove()}>Remove</button>
-            </div>
         </div>
     )
  }
