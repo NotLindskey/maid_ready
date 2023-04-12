86d74a502f0141034d6257d1f8655ffddf1f2be6
Peter Lee <PeterLee2094@gmail.com>
2023-04-12 16:38:51 -0500

    created get all job get request

--- server/routes/job.router.js
+++ server/routes/job.router.js
@@ -1,18 +1,50 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 /**
- * GET route template
+ * GET route
  */
-router.get('/', (req, res) => {
+
+// get all jobs
+router.get("/", (req, res) => {
+  if (req.isAuthenticated()) {
+    const query = `SELECT 
+        "user"."username", 
+        "property"."street",
+        "property"."city",
+        "property"."state",
+        "property"."zipcode", 
+        "job"."price",
+        "job"."date_completed_by" 
+        FROM "job"
+        JOIN "user" 
+        ON "job"."owner_id" = "user"."id"
+        JOIN "property" 
+        ON "property"."id" = "job"."property_id";`;
+    pool
+      .query(query)
+      .then((result) => {
+        res.send(result.rows);
+      })
+      .catch((error) => {
+        console.log(`Error getting properties`, error);
+        res.sendStatus(500);
+      });
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+// GET job by ID
+router.get("/", (req, res) => {
   // GET route code here
 });
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // POST route code here
 });
 
