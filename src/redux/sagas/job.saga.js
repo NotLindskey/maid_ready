bd1bb22b9fba370c914bfd0fc64932613bd887ce
Peter Lee <PeterLee2094@gmail.com>
2023-04-12 16:31:42 -0500

    added saga function

---
+++ src/redux/sagas/job.saga.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { put, takeLatest } from "redux-saga/effects";
+
+// watcher function
+function* jobSaga() {
+  yield takeLatest("FETCH_JOBS", fetchJobs);
+  yield takeLatest("FETCH_JOB", fetchJob);
+}
+
+function* fetchJobs() {
+  try {
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+
+    yield put({ type: 'SET_JOBS'});
+  } catch (err) {
+    console.log("Error with fetching jobs: ", err);
+  }
+}
+
+function* fetchJob() {
+    try {
+      const config = {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      };
+  
+      yield put({ type: 'SET_JOB'});
+    } catch (err) {
+      console.log("Error with fetching jobs: ", err);
+    }
+}
+
+export default jobSaga;
