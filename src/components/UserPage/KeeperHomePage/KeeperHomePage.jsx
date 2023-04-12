86fa76baf9325ea219db47d4874580d5cfbe8a1e
Peter Lee <PeterLee2094@gmail.com>
2023-04-11 19:09:26 -0500

    created keeper home page 'template' and added class names

--- src/components/UserPage/KeeperHomePage/KeeperHomePage.jsx
+++ src/components/UserPage/KeeperHomePage/KeeperHomePage.jsx
@@ -1,11 +1,25 @@
-import './KeeperHomePage.css'
+import "./KeeperHomePage.css";
 
-function KeeperHomePage(){
-    return(
-        <div>
-            <button><p>Find A Job</p></button>
-        </div>
-    )
+function KeeperHomePage() {
+  return (
+    <div className="keeper-home-page-body">
+      <div className="keeper-home-page-find-job-container">
+        <button className="keeper-home-page-find-job-button">
+          <p>Find A Job</p>
+        </button>
+      </div>
+
+      {/* conditonal rendering */}
+      {/* active job */}
+      <div className="keeper-home-page-active-job"></div>
+
+      {/* previous jobs */}
+      <div className="keeper-home-page-previous-job"></div>
+
+      {/* applied jobs */}
+      <div className="keeper-home-page-applied-job"></div>
+    </div>
+  );
 }
 
 export default KeeperHomePage;
