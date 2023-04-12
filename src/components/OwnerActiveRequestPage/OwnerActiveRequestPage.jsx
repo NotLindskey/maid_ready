de1eb90890ed8190d5a1551d0c935abb673b0620
Linssi Colin Ruiz <linssi101@gmail.com>
2023-04-11 19:39:37 -0500

    created OwnersActiveRequestPage html skeleton

---
+++ src/components/OwnerActiveRequestPage/OwnerActiveRequestPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import OwnerActiveRequest from '../OwnerActiveRequest/OwnerActiveRequest';
+
+function OwnerActiveRequestList() {
+  console.log('in the OwnerActiveRequestPage');
+
+  return (
+    <div>
+      <OwnerActiveRequest />
+    </div>
+  );
+}
+
+export default OwnerActiveRequestList;
