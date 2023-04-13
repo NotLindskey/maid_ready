63cc67ea261e0c1389b244a4dbf1aa78aa470ee3
Peter Lee <PeterLee2094@gmail.com>
2023-04-13 12:37:28 -0500

    imported and created an instance of all dependencies

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,6 +1,19 @@
 import './JobDetails.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function JobDetails(){
+    const params = useParams();
+    const jobId = params.id;
+
+    const dispatch = useDispatch();
+    const details = useSelector(store=>store.job.job_detail);
+
+    useEffect(()=>{
+
+    },[])
+
     return(
         <div>job details page</div>
     )
