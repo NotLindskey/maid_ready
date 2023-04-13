5d49624cd60ea3f2108acaab4dfe0580a8f6e3ff
Linssi Colin Ruiz <linssi101@gmail.com>
2023-04-12 22:09:51 -0500

    created OwnerRequestDetails component

---
+++ src/components/OwnerRequestDetails/OwnerRequestDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function OwnerRequestDetails() {
+  console.log('inside OwnerRequestDetails, hello world :)');
+
+  return (
+    <div className="request-details">
+      <div className="request-details-header"> Request Details by ID</div>
+    </div>
+  );
+}
+
+export default OwnerRequestDetails;
