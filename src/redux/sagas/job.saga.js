650f141d034beb530f41498d7d50c74e861903d0
Peter Lee <PeterLee2094@gmail.com>
2023-04-12 19:53:08 -0500

    added saga function

--- src/redux/sagas/job.saga.js
+++ src/redux/sagas/job.saga.js
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { put, takeLatest } from "redux-saga/effects";
+import { put, take, takeLatest } from "redux-saga/effects";
 
 // watcher function
 function* jobSaga() {
   yield takeLatest("FETCH_JOBS", fetchJobs);
   yield takeLatest("FETCH_JOB", fetchJob);
+  yield takeLatest("FETCH_JOB_DETAIL", fetchJobDetail);
 }
 
 // fetch all jobs
@@ -15,9 +16,8 @@ function* fetchJobs() {
       withCredentials: true,
     };
 
-    const jobsData = yield axios.get('/api/job')
-    console.log(jobsData.data)
-    yield put({ type: 'SET_JOBS', payload: jobsData.data });
+    const jobsData = yield axios.get("/api/job");
+    yield put({ type: "SET_JOBS", payload: jobsData.data });
   } catch (err) {
     console.log("Error with fetching jobs: ", err);
   }
@@ -25,16 +25,26 @@ function* fetchJobs() {
 
 // fetch job by id
 function* fetchJob() {
-    try {
-      const config = {
-        headers: { "Content-Type": "application/json" },
-        withCredentials: true,
-      };
-  
-      yield put({ type: 'SET_JOB'});
-    } catch (err) {
-      console.log("Error with fetching jobs: ", err);
-    }
+  try {
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+
+    yield put({ type: "SET_JOB" });
+  } catch (err) {
+    console.log("Error with fetching jobs: ", err);
+  }
+}
+
+// fetch job detail
+function* fetchJobDetail(action) {
+  try {
+    const jobDetail = yield axios.get(`/api/job/detail/${action.payload.id}`);
+    yield put({ type: "SET_JOB_DETAIL", payload: jobDetail.data });
+  } catch (err) {
+    console.log("Error with getting job detail: ", err);
+  }
 }
 
 export default jobSaga;
