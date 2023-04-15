59b6576b512bcb129d2e71010107d228ee1b2543
Peter Lee <PeterLee2094@gmail.com>
2023-04-14 23:23:18 -0500

    created Keeper activity page and css page

---
+++ src/components/KeeperActivityPage/KeeperActivityPage.jsx
@@ -0,0 +1,13 @@
+import styles from './KeeperActivityPage.css'
+
+function KeeperActivityPage(){
+    return(
+        <div className='keeper-activity-page-body'>
+            <div className='keeper-activity-active'></div>
+            <div className='keeper-activity-accepted'></div>
+            <div className='keeper-activity-previous'></div>
+        </div>
+    )
+}
+
+export default KeeperActivityPage;
