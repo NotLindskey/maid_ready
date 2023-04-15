d5e7ec14eb103cacb2c683546c6234571739b008
Peter Lee <PeterLee2094@gmail.com>
2023-04-14 20:43:43 -0500

    added update route to server

--- server/routes/job.router.js
+++ server/routes/job.router.js
@@ -66,11 +66,12 @@ router.get("/detail/:id", (req, res) => {
         JOIN "property" 
         ON "property"."id" = "job"."property_id"
         WHERE "job"."id" = $1;`;
-    pool.query(query, [req.params.id])
-    .then((result)=>{
+    pool
+      .query(query, [req.params.id])
+      .then((result) => {
         res.send(result.rows);
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         console.log(`Error getting job detail: `, error);
         res.sendStatus(500);
       });
@@ -80,23 +81,64 @@ router.get("/detail/:id", (req, res) => {
 });
 
 /**
- * POST route 
+ * POST route
  */
 router.post("/", (req, res) => {
   if (req.isAuthenticated()) {
     const query = `INSERT INTO "job" ("price","date_completed_by", "time", "status", "claimed", "property_id", "owner_id")
                     VALUES ($1, $2, $3, $4, $5, $6, $7)`;
-      pool.query(query, [req.body.price, req.body.date_completed_by, req.body.time, req.body.status, req.body.claimed, req.body.property_id, req.user.id])
-        .then(() => {
-          res.sendStatus(201);
-        })
-        .catch(err => {
-          console.log('ERROR: Posting job', err);
-          res.sendStatus(500)
-        })
-    } else {
-      res.sendStatus(403);
-    }
+    pool
+      .query(query, [
+        req.body.price,
+        req.body.date_completed_by,
+        req.body.time,
+        req.body.status,
+        req.body.claimed,
+        req.body.property_id,
+        req.user.id,
+      ])
+      .then(() => {
+        res.sendStatus(201);
+      })
+      .catch((err) => {
+        console.log("ERROR: Posting job", err);
+        res.sendStatus(500);
+      });
+  } else {
+    res.sendStatus(403);
+  }
 });
 
+/**
+ * UPDATE route
+ */
+router.put("/apply/:id", (req, res) => {
+  if (req.isAuthenticated()) {
+    // you will need the job id and the keeper id
+    const query = `
+    WITH cte AS (
+      SELECT "id"
+      FROM "job"
+      WHERE "claimed" = FALSE AND "keeper_id" IS NULL
+      LIMIT 1
+    )
+    UPDATE "job" c
+    SET "claimed" = TRUE, "keeper_id" = $1
+    FROM cte 
+    WHERE c."id" = $2
+    RETURNING *;
+    `;
+
+    pool
+      .query(query, [req.body.jobId, req.body.keeperId])
+      .then(() => {
+        res.sendStatus(201);
+      })
+      .catch((err) => {
+        console.log("Error with applying to job: ", err);
+      });
+  } else {
+    res.sendStatus(403);
+  }
+});
 module.exports = router;
