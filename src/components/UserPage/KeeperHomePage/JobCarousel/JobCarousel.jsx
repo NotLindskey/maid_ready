2671f069dfad1d9e5a0359593f85f4a754320a0d
Peter Lee <PeterLee2094@gmail.com>
2023-04-15 16:08:42 -0500

    render the job data onto the page

--- src/components/UserPage/KeeperHomePage/JobCarousel/JobCarousel.jsx
+++ src/components/UserPage/KeeperHomePage/JobCarousel/JobCarousel.jsx
@@ -15,16 +15,30 @@ to use:
 */
 import "./JobCarousel.css";
 import JobItem from "../../../JobItem/JobItem";
-function JobCarousel({jobs}) {
-
+function JobCarousel({ jobs }) {
   return (
     <div className="job-carousel-body">
       {/* <button className="job-carousel-left-button">left</button> */}
       <div className="job-carousel-job-listing-container">
         <div className="job-carousel-job-listing-container-sub">
-          {jobs ? jobs.map((job)=>(
-            <JobItem width={20} key={job.id}/>
-          )) : <p> no jobs </p>}
+          {jobs ? (
+            jobs.map((job) => (
+              <JobItem
+                width={20}
+                key={job.id}
+                id={job.id}
+                owner={job.username}
+                street={job.street}
+                city={job.city}
+                state={job.state}
+                zip={job.zipcode}
+                price={job.price}
+                date={job.date_completed_by}
+              />
+            ))
+          ) : (
+            <p> no jobs </p>
+          )}
         </div>
       </div>
       {/* <button className="job-carousel-right-button">right</button> */}
