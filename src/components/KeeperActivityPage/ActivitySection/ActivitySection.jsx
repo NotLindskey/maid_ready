547c3c1e9cfe223545e9fce3450914295e56704c
Peter Lee <PeterLee2094@gmail.com>
2023-04-15 16:44:39 -0500

    created activity section with applied jobs

--- src/components/KeeperActivityPage/ActivitySection/ActivitySection.jsx
+++ src/components/KeeperActivityPage/ActivitySection/ActivitySection.jsx
@@ -1,3 +1,4 @@
+import JobItem from "../../JobItem/JobItem";
 function ActivitySection({ title, jobs }) {
   return (
     <div>
@@ -5,19 +6,30 @@ function ActivitySection({ title, jobs }) {
         <p>{title}</p>
       </div>
       <div>
-        {jobs ? (
-          jobs.length > 0 ? (
-            {
-                // jobs.map((job)=>(
-
-                // ))
-            }
-          ) : (
+        {jobs.map((job) => (
+          <JobItem
+            width={20}
+            key={job.id}
+            id={job.id}
+            owner={job.username}
+            street={job.street}
+            city={job.city}
+            state={job.state}
+            zip={job.zipcode}
+            price={job.price}
+            date={job.date_completed_by}
+          />
+        ))}
+        {/* {jobs ? (=
+          jobs.length > 0 ?
+          {
+            jobs.map((jobs)=>{return()})
+          } : (
             <p>no jobs found</p>
           )
         ) : (
           <p>no jobs found</p>
-        )}
+        )} */}
       </div>
     </div>
   );
--- src/components/KeeperActivityPage/KeeperActivityPage.jsx
+++ src/components/KeeperActivityPage/KeeperActivityPage.jsx
@@ -1,4 +1,5 @@
 import styles from "./KeeperActivityPage.css";
+import ActivitySection from "./ActivitySection/ActivitySection";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 function KeeperActivityPage() {
@@ -6,14 +7,20 @@ function KeeperActivityPage() {
   const userJobs = useSelector((store) => store.job.user_jobs);
 
   useEffect(() => {
-      dispatch({ type: "FETCH_USER_JOBS" });
+    dispatch({ type: "FETCH_USER_JOBS" });
   }, []);
 
   return (
     <div className="keeper-activity-page-body">
       <div className="keeper-activity-active"></div>
-      <div className="keeper-activity-accepted"></div>
-      <div className="keeper-activity-previous"></div>
+      <ActivitySection
+        jobs={userJobs.filter((job) => job.status !== "incomplete")}
+        title={"previous jobs"}
+      />
+      <ActivitySection
+        jobs={userJobs.filter((job) => job.status === "incomplete")}
+        title={"Applied Jobs"}
+      />
     </div>
   );
 }
