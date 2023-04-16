4edfbff76b42fb840b9db1b4bf8c0bb934722439
Linssi Colin Ruiz <linssi101@gmail.com>
2023-04-16 12:39:36 -0500

    created view completed request page to view completed requests

--- src/components/OwnerCompletedRequestPage/OwnerCompletedRequestPage.jsx
+++ src/components/OwnerCompletedRequestPage/OwnerCompletedRequestPage.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
-import OwnerActiveRequest from '../OwnerActiveRequest/OwnerActiveRequest';
-import OwnerCompletedRequest from '../OwnerCompletedRequest/OwnerCompletedRequest';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+import JobItem from '../JobItem/JobItem';
 
 function OwnerCompletedRequestPage() {
   console.log('in the OwnerCompletedRequestPage');
+  const dispatch = useDispatch();
+  const jobs = useSelector((store) => store.job.jobs);
+
+  useEffect(() => {
+    dispatch({ type: 'FETCH_JOBS' });
+  }, []);
 
   return (
     <div>
-      <p>Completed Requests:</p>
-      <OwnerCompletedRequest />
+      <p>Completed Requests List:</p>
+      <p>
+        <strong>
+          `get route to display jobItem only by: status = completed && ORDER BY
+          date_completed_by`
+        </strong>
+      </p>
+      <div className="job-list-body">
+        <div className="job-list-container">
+          {jobs.length ? (
+            jobs.map((job) => (
+              <div>
+                <JobItem
+                  key={job.id}
+                  id={job.id}
+                  owner={job.username}
+                  street={job.street}
+                  city={job.city}
+                  state={job.state}
+                  zip={job.zipcode}
+                  price={job.price}
+                  date={job.date_completed_by}
+                />
+              </div>
+            ))
+          ) : (
+            <p>no jobs found</p>
+          )}
+        </div>
+      </div>{' '}
     </div>
   );
 }
