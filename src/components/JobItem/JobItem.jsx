e4d8e389c355c5a72daf1818b95a710bcafb623d
Rox Niederk <rox.niederkorn@gmail.com>
2023-04-18 20:45:20 -0500

    put in the date prop into JobItem.jsx and added more btn classes

--- src/components/JobItem/JobItem.jsx
+++ src/components/JobItem/JobItem.jsx
@@ -25,11 +25,11 @@ function JobItem({ width, owner, street, city, state, zip, price, date, id }) {
           {city}, {state} {zip}
         </p>
       </div>
-      <div className="job-item-distance">
+      {/* <div className="job-item-distance">
         <p>3.1 miles away</p>
-      </div>
+      </div> */}
       <div className="job-item-due-date">
-        <p>March 23 - March 28</p>
+        <p>{date}</p>
       </div>
       <div className="job-item-price">
         <p>${price}</p>
