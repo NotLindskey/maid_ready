1adc63dcfbd7972758a6558f55434886b6384469
Peter Lee <PeterLee2094@gmail.com>
2023-04-18 20:42:02 -0500

    added keeper job complettion page

--- src/components/KeeperJobCompletion/KeeperJobCompletion.jsx
+++ src/components/KeeperJobCompletion/KeeperJobCompletion.jsx
@@ -1,7 +1,51 @@
 import "./KeeperJobCompletion.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useHistory } from "react-router-dom";
+import { useEffect } from "react";
 
 function KeeperJobCompletion() {
-  return <div>complete job</div>;
+  const params = useParams();
+  const history = useHistory();
+  const jobId = params.id;
+
+  const dispatch = useDispatch();
+  const details = useSelector((store) => store.job.job_detail);
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_JOB_DETAIL", payload: { id: jobId } });
+  }, []);
+  return (
+    <div className="job-details-body">
+      <div className="job-details-overview">
+        <div className="job-detail-title">
+          <p>Overview</p>
+        </div>
+        <div className="job-detail-info">
+          <div className="job-detail-name">
+            <p>{details.username}</p>
+          </div>
+          <div className="job-detail-location">
+            <p>
+              {details.street} {details.city}, {details.state} {details.zipcode}
+            </p>
+          </div>
+          <div className="job-detail-date">
+            <p>{details.date_completed_by}</p>
+          </div>
+          <div className="job-detail-price">
+            <p>${details.price}</p>
+          </div>
+        </div>
+        <button className="btn">Complete</button>
+      </div>
+      <div className="job-details-checklist">
+        <div className="job-detail-title">
+          <p>Checklist</p>
+        </div>
+        <div className="job-detail-checklist-container"></div>
+      </div>
+    </div>
+  );
 }
 
 export default KeeperJobCompletion;
