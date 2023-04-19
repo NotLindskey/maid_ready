95077bb04e831daf47412a672fbfc02bc21b6260
Tina Tram <tinatram07@gmail.com>
2023-04-19 09:58:12 -0500

    edited onclick function for view request handler

--- src/components/OwnerActiveRequest/OwnerActiveRequest.jsx
+++ src/components/OwnerActiveRequest/OwnerActiveRequest.jsx
@@ -19,7 +19,6 @@ function OwnerActiveRequest() {
   }, []);
 
   const handleViewRequest = (request) => {
-    console.log('handleViewRequest clicked!');
     console.log(request.id);
     history.push(`/OwnerRequestDetails/${request.id}`);
   };
@@ -52,8 +51,7 @@ function OwnerActiveRequest() {
                   </p>
                   <p>${request.price}</p>
 
-                  <button className="btn" onClick={handleViewRequest}>View</button>
-
+                  <button className="btn" onClick={() => handleViewRequest(request)}>View</button>
                   <button className="btn">Delete</button>
                 </div>
               );
