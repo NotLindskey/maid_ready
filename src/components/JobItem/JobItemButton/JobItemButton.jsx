25b363384927b413e285f994d82033089ae1c01a
Peter Lee <PeterLee2094@gmail.com>
2023-04-18 20:02:46 -0500

    added data to all placed where jobItem is renderd

--- src/components/JobItem/JobItemButton/JobItemButton.jsx
+++ src/components/JobItem/JobItemButton/JobItemButton.jsx
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import "./JobItemButton.css";
 
-function JobItemButton({ status, claimed, keeper, type }) {
+function JobItemButton({ status, claimed, keeper }) {
   useEffect(() => {
     console.log(`
         status: ${status} \n
         clamied: ${claimed} \n
         keeper: ${keeper} \n
-        type: ${type}
+        type: ${userType}
         `);
   });
+
+  const userType = useSelector((store) => store.user.account_type);
   return (
     <button className="btn job-item-accept-button">condition button</button>
   );
--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -1,41 +1,44 @@
-import './JobList.css';
-import JobItem from '../JobItem/JobItem';
+import "./JobList.css";
+import JobItem from "../JobItem/JobItem";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-function JobList(){
-    const dispatch = useDispatch();
-    const jobs = useSelector(store=>store.job.jobs);
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+function JobList() {
+  const dispatch = useDispatch();
+  const jobs = useSelector((store) => store.job.jobs);
 
-    useEffect(()=>{
-        dispatch({type: "FETCH_JOBS"})
-    },[])
-    return(
-        <div className='job-list-body'>
-            <div className='job-list-container'>
-                { jobs.length ?
-                    jobs.map((job)=>(
-                        <JobItem
-                        key={job.id}
-                        id={job.id}
-                        owner={job.username}
-                        street={job.street}
-                        city={job.city}
-                        state={job.state}
-                        zip={job.zipcode}
-                        price={job.price}
-                        date={job.date_completed_by}
-                        />
-                    ))
-
-                    :
-
-                    <p>no jobs found</p>
-                }
-            </div>
-
-        </div>
-    )
+  useEffect(() => {
+    dispatch({ type: "FETCH_JOBS" });
+  }, []);
+  return (
+    <div className="job-list-body">
+      <div className="job-list-container">
+        {jobs.length ? (
+          jobs.map((job) => (
+            <JobItem
+              // defeault value
+              key={job.id}
+              // display option
+              id={job.id}
+              owner={job.username}
+              street={job.street}
+              city={job.city}
+              state={job.state}
+              zip={job.zipcode}
+              price={job.price}
+              date={job.date_completed_by}
+              // button
+              claimed={job.claimed}
+              status={job.status}
+              keeper_id={job.keeper_id}
+            />
+          ))
+        ) : (
+          <p>no jobs found</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default JobList;
