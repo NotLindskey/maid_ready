144cb4d14c7b43adf31e09ef4213e01123427310
Peter Lee <PeterLee2094@gmail.com>
2023-04-20 17:11:49 -0500

    added modal when clicking add task

--- src/components/CreateJobForm/JobFormCheckList/JobFormCheckList.jsx
+++ src/components/CreateJobForm/JobFormCheckList/JobFormCheckList.jsx
@@ -1,6 +1,7 @@
 import "./JobFormCheckList.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
+import CompletionModal from "../../KeeperJobCompletion/CompletionModal/CompletionModal";
 
 function JobFormCheckList({ standards }) {
   const dispatch = useDispatch();
@@ -12,6 +13,15 @@ function JobFormCheckList({ standards }) {
     standards.reduce((arr, task) => [...arr, task.task], [])
   );
 
+  const [customValues, setCustomValues] = useState([]);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  function handleButtonClick() {
+    setIsModalOpen(true);
+  }
+  function handleCloseModal() {
+    setIsModalOpen(false);
+  }
   const handleCheckboxChange = (event) => {
     const value = event.target.value;
     const isChecked = event.target.checked;
@@ -21,7 +31,7 @@ function JobFormCheckList({ standards }) {
       setCheckedValues(checkedValues.filter((val) => val !== value));
     }
 
-    console.log(checkedValues); // Outputs an array of the checked values
+    // console.log(checkedValues); // Outputs an array of the checked values
   };
 
   useEffect(() => {
@@ -37,11 +47,49 @@ function JobFormCheckList({ standards }) {
               value={standard.task}
               checked={checkedValues.includes(standard.task)}
               onChange={handleCheckboxChange}
+              id={`standard-${standard.id}`}
             />
-            <label>{standard.task}</label>
+            <label for={`standard-${standard.id}`}>{standard.task}</label>
           </div>
         );
       })}
+
+      <div className="checklist-custom">
+        <button onClick={handleButtonClick} className="btn" type="button">
+          add task
+        </button>
+      </div>
+
+      <CompletionModal
+        isModalOpen={isModalOpen}
+        onCloseModal={handleCloseModal}
+        title="add task"
+      >
+        <div
+          style={{
+            width: "40rem",
+            height: "7rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div className="input-container">
+            <input
+              type="text"
+              id="username"
+              className="text-input"
+              autocomplete="off"
+              placeholder="Enter task"
+              required
+            />
+            <label for="username" className="label">
+              Enter task
+            </label>
+          </div>
+          <button>add</button>
+        </div>
+      </CompletionModal>
     </div>
   );
 }
