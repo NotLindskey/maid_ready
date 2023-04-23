6bee690cfbcd06ad75a94f4bb99537a936a8f482
Linssi Colin Ruiz <linssi101@gmail.com>
2023-04-23 15:13:49 -0500

    added styling to OwnerRequestDetails

--- src/components/OwnerRequestDetails/OwnerRequestDetails.jsx
+++ src/components/OwnerRequestDetails/OwnerRequestDetails.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import { useDispatch, useSelector } from "react-redux";
-import { useParams, useHistory } from "react-router-dom";
-import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams, useHistory } from 'react-router-dom';
+import { useEffect } from 'react';
+import './OwnerRequestDetails.css';
 
 function OwnerRequestDetails() {
   const params = useParams();
@@ -12,19 +13,19 @@ function OwnerRequestDetails() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-  //   dispatch({ type: "FETCH_JOB_DETAIL", payload: {id: requestId} });
-  // }, []);
-  dispatch({ type: "FETCH_REQUEST_DETAIL", payload: {id: requestId} });
+    //   dispatch({ type: "FETCH_JOB_DETAIL", payload: {id: requestId} });
+    // }, []);
+    dispatch({ type: 'FETCH_REQUEST_DETAIL', payload: { id: requestId } });
   }, []);
 
   const goBack = () => {
-    history.push('/OwnerViewRequestsPage')
-  }
+    history.push('/OwnerViewRequestsPage');
+  };
 
   return (
     <div className="request-details">
       <div className="request-details-content">
-        <div className="request-details-header"> 
+        <div className="request-details-header">
           <h2>Request Details</h2>
         </div>
 
@@ -32,19 +33,46 @@ function OwnerRequestDetails() {
 
         <div className="grid">
           <div className="grid-content-section">
-            <div className="request-details-image"></div>
             <div className="request-details-text">
-              <p>{requestDetails.username}</p>
-              <p>{requestDetails.street} {requestDetails.city} {requestDetails.state} {requestDetails.zipcode}</p>
-              <p>{new Date(requestDetails.date_completed_by).toLocaleDateString('en-US')}</p>
-              <p>${requestDetails.price}</p>
-              {requestDetails.claimed && requestDetails.status === 'complete' ? <p>Status: Completed by Keeper</p>  
-              : requestDetails.claimed && requestDetails.status !== 'complete' ? <p>Status: Claimed by Keeper</p>
-              : <p>Status: Not Claimed</p>}
+              <p>
+                <strong>User: </strong>
+                {requestDetails.username}
+              </p>
+              <p>
+                <strong>Address:</strong> {requestDetails.street}{' '}
+                {requestDetails.city} {requestDetails.state}{' '}
+                {requestDetails.zipcode}
+              </p>
+              <p>
+                <strong>Date: </strong>
+                {new Date(requestDetails.date_completed_by).toLocaleDateString(
+                  'en-US',
+                )}
+              </p>
+              <p>
+                <strong>Price:</strong> ${requestDetails.price}
+              </p>
+              {requestDetails.claimed &&
+              requestDetails.status === 'complete' ? (
+                <p>
+                  <strong>Status:</strong> Completed by Keeper
+                </p>
+              ) : requestDetails.claimed &&
+                requestDetails.status !== 'complete' ? (
+                <p>
+                  <strong>Status:</strong> Claimed by Keeper
+                </p>
+              ) : (
+                <p>
+                  <strong>Status:</strong> Not Claimed
+                </p>
+              )}
             </div>
             <div className="grid-button-section">
               <div className="request-details-cancel">
-                <button className="btn" onClick={() => goBack()}>Back</button>
+                <button className="btn" onClick={() => goBack()}>
+                  Back
+                </button>
               </div>
             </div>
           </div>
