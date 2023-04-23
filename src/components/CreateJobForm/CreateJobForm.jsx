d285347025f9e37bcbe6bc687ab4af448e5a2258
Tina Tram <tinatram07@gmail.com>
2023-04-23 07:35:33 -0500

    created css file for create job form, centered div

--- src/components/CreateJobForm/CreateJobForm.jsx
+++ src/components/CreateJobForm/CreateJobForm.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import JobFormCheckList from "./JobFormCheckList/JobFormCheckList";
+import './CreateJobForm.css'
+
 function CreateJobForm(props) {
   const property = useSelector((store) => store.property.property);
   const cleaningStandard = useSelector(
