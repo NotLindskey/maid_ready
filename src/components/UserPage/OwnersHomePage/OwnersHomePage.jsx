47d1651adba9b23d521df378a20b4caf2c1f3dd6
Linssi Colin Ruiz <linssi101@gmail.com>
2023-04-24 06:14:52 -0500

    added history.push to images when clicked. added box shadow to images and changed colors to buttons

--- src/components/UserPage/OwnersHomePage/OwnersHomePage.jsx
+++ src/components/UserPage/OwnersHomePage/OwnersHomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
-import requestImg from './Request.png'; 
+import { useSelector } from 'react-redux';
+import requestImg from './Request.png';
 import viewImg from './View.png';
 import { useHistory } from 'react-router';
 import OwnersHomePageStyle from './OwnersHomePage.css';
@@ -15,24 +15,42 @@ function OwnersHomePage() {
   const toProperties = () => {
     console.log('Button clicked to go to Properties Page');
     history.push('/properties');
-  }
+  };
 
   const viewRequests = () => {
-    console.log('This will go to Owner Requests page')
+    console.log('This will go to Owner Requests page');
     history.push('/OwnerViewRequestsPage');
-  }
+  };
 
   return (
     <div>
       {/* <UserPage /> */}
-      <h2 className='heading'>{heading}</h2>
-      <div className='container1'>
-        <img src={requestImg} alt="image of two cleaners with cleaning tools" width='500' height='375'/>
-        <button className='btn1' onClick={toProperties}>Create A Request</button>
+      <h2 className="heading">{heading}</h2>
+      <div className="container1">
+        <img
+          className="box-container"
+          onClick={() => history.push('/properties')}
+          src={requestImg}
+          alt="image of two cleaners with cleaning tools"
+          width="500"
+          height="375"
+        />
+        <button className="btn1" onClick={toProperties}>
+          Create A Request
+        </button>
       </div>
-      <div className='container1'>
-        <img src={viewImg} alt="image of a person writing in a planner" width='500' height='375'/>
-        <button className='btn1' onClick={viewRequests}>View Requests</button>
+      <div className="container1">
+        <img
+          className="box-container"
+          onClick={() => history.push('/OwnerViewRequestsPage')}
+          src={viewImg}
+          alt="image of a person writing in a planner"
+          width="500"
+          height="375"
+        />
+        <button className="btn1" onClick={viewRequests}>
+          View Requests
+        </button>
       </div>
     </div>
   );
